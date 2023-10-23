ee3e059290a4c6da23abb4bd3ab7b08135a75663
Ilya K <121868806+Lio-Kay@users.noreply.github.com>
2023-10-24 06:27:27 +0700

    fix: wrote user views

--- habits/accounts/views.py
+++ habits/accounts/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import generics
 
-# Create your views here.
+from accounts.serializers import UserSerializer
+
+
+User = get_user_model()
+
+
+class UserListCreateAPIView(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
