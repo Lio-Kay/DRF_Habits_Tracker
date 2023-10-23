8144e6450e9bca431b20217ae3028ddcb8063abb
Ilya K <121868806+Lio-Kay@users.noreply.github.com>
2023-10-24 02:01:07 +0700

    feat: created habit generic views

--- habits/habits_app/views.py
+++ habits/habits_app/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from habits_app.models import Habit
+from habits_app.serializers import HabitSerializer
+
+
+class HabitListCreateAPIView(generics.ListCreateAPIView):
+    """Вьюсет на создание и вывода списка привычек"""
+    queryset = Habit.objects.all()
+    serializer_class = HabitSerializer
+
+
+class HabitRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    """Вьюсет на детальный вывод, редактирование, удаление привычки"""
+    queryset = Habit.objects.all()
+    serializer_class = HabitSerializer
