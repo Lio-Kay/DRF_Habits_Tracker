a71e29391974ba247364ce73085be2e9e578bde8
Ilya K <121868806+Lio-Kay@users.noreply.github.com>
2023-10-25 06:41:18 +0700

    feat: added IsOwner perm for accounts feat: added owner filter to UserListCreateAPIView

--- habits/accounts/views.py
+++ habits/accounts/views.py
@@ -3,6 +3,7 @@ from rest_framework import generics, status
 from rest_framework.response import Response
 
 from accounts.serializers import UserSerializer
+from accounts.permissions import IsOwner
 
 
 User = get_user_model()
@@ -22,7 +23,11 @@ class UserListCreateAPIView(generics.ListCreateAPIView):
 
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
+    def get_queryset(self):
+        return User.objects.all().filter(pk=self.request.user.pk)
+
 
 class UserRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [IsOwner]
