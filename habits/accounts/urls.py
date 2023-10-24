f88d56a9b8dc5e66caf2a037bfa801d873f16c5b
Ilya K <121868806+Lio-Kay@users.noreply.github.com>
2023-10-25 01:42:48 +0700

    feat: set up JWT auth

--- habits/accounts/urls.py
+++ habits/accounts/urls.py
@@ -1,4 +1,9 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 from accounts.apps import AccountsConfig
 from accounts.views import UserListCreateAPIView, UserRetrieveUpdateDestroyAPIView
@@ -7,6 +12,10 @@ from accounts.views import UserListCreateAPIView, UserRetrieveUpdateDestroyAPIVi
 app_name = AccountsConfig.name
 
 urlpatterns = [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+
     path('', UserListCreateAPIView.as_view(), name='user_list_create'),
     path('<int:pk>/', UserRetrieveUpdateDestroyAPIView.as_view(), name='user_retrieve_update_destroy'),
 ]
