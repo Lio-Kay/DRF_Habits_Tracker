b073b25c334b474f9865df4a45cc812ae6db0698
Ilya K <121868806+Lio-Kay@users.noreply.github.com>
2023-10-25 02:05:26 +0700

    feat: added create_user method to accounts

--- habits/accounts/views.py
+++ habits/accounts/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
 from django.contrib.auth import get_user_model
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
 
 from accounts.serializers import UserSerializer
 
@@ -11,6 +11,16 @@ User = get_user_model()
 class UserListCreateAPIView(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    def create(self, request, *args, **kwargs):
+        data = request.data
+        user = User.objects.create_user(
+            email=data.get('email', ''),
+            password=str(data.get('password', ''))
+        )
+        serializer = self.get_serializer(user)
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 class UserRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
