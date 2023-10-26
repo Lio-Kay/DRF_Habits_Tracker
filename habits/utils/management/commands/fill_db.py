f1417c093064c2ba1fb1dbd4168431d43162ad20
Ilya K <121868806+Lio-Kay@users.noreply.github.com>
2023-10-27 04:07:25 +0700

    feat: created fill db command

--- habits/utils/management/commands/fill_db.py
+++ habits/utils/management/commands/fill_db.py
@@ -10,7 +10,7 @@ User = get_user_model()
 
 class Command(BaseCommand):
 
-    help = 'Clear the DB and fills it with data'
+    help = 'Clears the DB and fills it with data'
 
     def handle(self, *args, **options):
 
@@ -22,39 +22,37 @@ class Command(BaseCommand):
         user1 = User.objects.create_user(email='abc@mail.com', tg_name=None, password='abc')
         user2 = User.objects.create_superuser(email='def@mail.com', tg_name=None, password='def')
 
-        habit1 = Habit.objects.create(action='Приятная публичная привычка', time="9:30:00",
-                                      duration='10', frequency='DLY',
-                                      created_at=None, place='Какое-то место',
+        habit1 = Habit.objects.create(action='Приятная публичная привычка',
+                                      time="9:30:00", duration='10', place='Какое-то место',
                                       is_pleasant=True, related_pleasant_habit=None,
                                       reward=None, creator=user1,
                                       is_public=True)
-        habit2 = Habit.objects.create(action='Приятная приватная привычка', time="10:30:00",
-                                      duration='20', frequency='2d',
-                                      created_at=None, place='Какое-то место 2',
+        habit2 = Habit.objects.create(action='Приятная приватная привычка',
+                                      time="10:30:00", duration='20', place='Какое-то место 2',
                                       is_pleasant=True, related_pleasant_habit=None,
                                       reward=None, creator=user2,
                                       is_public=True)
-        habit3 = Habit.objects.create(action='Полезная публичная привычка с приятной публичной привычкой', time="11:30:00",
-                                      duration='30', frequency='3d',
-                                      created_at=0, place='Какое-то место 3',
+        habit3 = Habit.objects.create(action='Полезная публичная привычка с приятной публичной привычкой',
+                                      time="11:30:00", duration='30', frequency='3d',
+                                      created_at='Monday', place='Какое-то место 3',
                                       is_pleasant=False, related_pleasant_habit=habit1,
                                       reward=None, creator=user1,
                                       is_public=True)
-        habit4 = Habit.objects.create(action='Полезная приватная привычка с приятной приватной привычкой', time="12:30:00",
-                                      duration='40', frequency='4d',
-                                      created_at=1, place='Какое-то место 4',
+        habit4 = Habit.objects.create(action='Полезная приватная привычка с приятной приватной привычкой',
+                                      time="12:30:00", duration='40', frequency='4d',
+                                      created_at='Tuesday', place='Какое-то место 4',
                                       is_pleasant=False, related_pleasant_habit=habit2,
                                       reward=None, creator=user1,
                                       is_public=False)
-        habit5 = Habit.objects.create(action='Полезная публичная привычка с вознаграждением', time="13:30:00",
-                                      duration='50', frequency='5d',
-                                      created_at=2, place='Какое-то место 5',
+        habit5 = Habit.objects.create(action='Полезная публичная привычка с вознаграждением',
+                                      time="13:30:00", duration='50', frequency='5d',
+                                      created_at='Wednesday', place='Какое-то место 5',
                                       is_pleasant=False, related_pleasant_habit=None,
                                       reward='Вознаграждение 1', creator=user2,
                                       is_public=True)
-        habit6 = Habit.objects.create(action='Полезная приватная привычка с вознаграждением', time="14:30:00",
-                                      duration='60', frequency='6d',
-                                      created_at=3, place='Какое-то место 6',
+        habit6 = Habit.objects.create(action='Полезная приватная привычка с вознаграждением',
+                                      time="14:30:00", duration='60', frequency='6d',
+                                      created_at='Thursday', place='Какое-то место 6',
                                       is_pleasant=False, related_pleasant_habit=None,
                                       reward='Вознаграждение 2', creator=user2,
                                       is_public=False)
